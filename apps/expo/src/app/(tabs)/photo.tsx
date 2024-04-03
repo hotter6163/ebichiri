06bd4414c30caef7802c04c43232a54169920b5b
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-04-03 10:47:33 +0900

    refactor

--- apps/expo/src/app/(tabs)/photo.tsx
+++ apps/expo/src/app/(tabs)/photo.tsx
@@ -1,50 +1,38 @@
 import type { FC } from "react";
 import type { NativeSafeAreaViewProps } from "react-native-safe-area-context";
-import { useEffect } from "react";
-import { Dimensions, FlatList, Image, View } from "react-native";
+import { Dimensions, FlatList, Image, Pressable } from "react-native";
+import { useRouter } from "expo-router";
 import { PageView } from "@/components/layout";
 import { api } from "@/utils/api";
 
 const SAFE_AREA: NativeSafeAreaViewProps["edges"] = ["top", "right", "left"];
 
-const Photo: FC = (props) => {
-  const { data, fetchNextPage } =
-    api.photo.getMineWithPagination.useInfiniteQuery(
-      {
-        limit: 3,
-      },
-      {
-        getNextPageParam: (lastPage) => lastPage.pagination.cursor,
-      },
-    );
-
-  useEffect(() => {
-    console.log(props);
-  }, [props]);
-
-  useEffect(() => {
-    data?.pages.forEach((page) => {
-      console.log(
-        page.items.length,
-        page.items[0]?.createdAt,
-        page.pagination.cursor,
-      );
-    });
-    setTimeout(() => {
-      void fetchNextPage();
-    }, 2000);
-  }, [data, fetchNextPage]);
+const Photo: FC = () => {
+  const router = useRouter();
+  const { data } = api.photo.getMineWithPagination.useInfiniteQuery(
+    {
+      limit: 30,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.pagination.cursor,
+    },
+  );
 
   const photos = data?.pages.flatMap((page) => page.items) ?? [];
-  const screenWidth = Dimensions.get("window").width;
-  const photoSize = screenWidth / 3;
+  const photoSize = Dimensions.get("window").width / 3;
 
   return (
     <PageView safeArea={SAFE_AREA}>
       <FlatList
         data={photos}
         renderItem={({ item }) => (
-          <View
+          <Pressable
+            onPress={() =>
+              router.push({
+                pathname: `/(photos)/[photoId]/`,
+                params: { photoId: item.id },
+              })
+            }
             style={{ width: photoSize, height: photoSize }}
             className="p-[1px]"
           >
@@ -52,7 +40,7 @@ const Photo: FC = (props) => {
               source={{ uri: item.src }}
               className="h-full w-full object-cover"
             />
-          </View>
+          </Pressable>
         )}
         numColumns={3}
       />
