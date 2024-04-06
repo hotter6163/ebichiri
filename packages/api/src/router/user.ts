879c2b47806d02d4debaf9f3184696b2c06eea76
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-04-06 12:01:40 +0900

    Update user.ts

--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -1,9 +1,22 @@
-import { user } from "@ebichiri/db/schema";
+import { eq } from "@ebichiri/db";
+import { user as userDbSchema } from "@ebichiri/db/schema";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
-  create: protectedProcedure.mutation(({ ctx }) =>
-    ctx.db.insert(user).values({ id: ctx.user.id }),
+  getMine: protectedProcedure.query(({ ctx }) =>
+    ctx.db
+      .select()
+      .from(userDbSchema)
+      .where(eq(userDbSchema.id, ctx.user.id))
+      .then((rows) => convertUser(rows[0] as typeof userDbSchema.$inferSelect)),
   ),
+  create: protectedProcedure.mutation(({ ctx }) =>
+    ctx.db.insert(userDbSchema).values({ id: ctx.user.id }),
+  ),
+});
+
+const convertUser = (user: typeof userDbSchema.$inferSelect) => ({
+  ...user,
+  name: user.name || "匿名ユーザー",
 });
