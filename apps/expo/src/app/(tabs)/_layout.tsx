0de2e442926d1eb8f2f52372e755f0a9f2f16ace
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-04-07 21:13:26 +0900

    Update _layout.tsx

--- apps/expo/src/app/(tabs)/_layout.tsx
+++ apps/expo/src/app/(tabs)/_layout.tsx
@@ -1,77 +1,65 @@
 import type { FC } from "react";
-import { Image } from "react-native";
 import { Tabs } from "expo-router";
 import { Header } from "@/components/header";
 import { PRIMARY_COLOR, SUB_BASE_COLOR } from "@/constants/colors";
-import { api } from "@/utils/api";
 import { Entypo, Feather, FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 
 const TAB_BAR_INACTIVE_COLOR = "white";
 
-const TabLayout: FC = () => {
-  const { data } = api.user.getMine.useQuery();
-
-  return (
-    <Tabs
-      screenOptions={{
-        header: () => <Header />,
-        tabBarActiveTintColor: PRIMARY_COLOR,
-        tabBarInactiveTintColor: TAB_BAR_INACTIVE_COLOR,
-        tabBarStyle: { backgroundColor: SUB_BASE_COLOR },
+const TabLayout: FC = () => (
+  <Tabs
+    screenOptions={{
+      header: () => <Header />,
+      tabBarActiveTintColor: PRIMARY_COLOR,
+      tabBarInactiveTintColor: TAB_BAR_INACTIVE_COLOR,
+      tabBarStyle: { backgroundColor: SUB_BASE_COLOR },
+    }}
+  >
+    <Tabs.Screen
+      name="index"
+      options={{
+        tabBarIcon: ({ color }) => (
+          <FontAwesome size={24} name="home" color={color} />
+        ),
       }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={24} name="home" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="camera"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={24} name="camera" color={color} />
-          ),
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="photo"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Entypo name="images" size={24} color={color} />
-          ),
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="map"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="map-marked-alt" size={24} color={color} />
-          ),
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarIcon: ({ color }) =>
-            data?.avatar ? (
-              <Image
-                source={{ uri: data.avatar }}
-                style={{ width: 24, height: 24 }}
-              />
-            ) : (
-              <Feather name="user" size={24} color={color} />
-            ),
-          headerShown: false,
-        }}
-      />
-    </Tabs>
-  );
-};
+    />
+    <Tabs.Screen
+      name="camera"
+      options={{
+        tabBarIcon: ({ color }) => (
+          <FontAwesome size={24} name="camera" color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tabs.Screen
+      name="photo"
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Entypo name="images" size={24} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tabs.Screen
+      name="map"
+      options={{
+        tabBarIcon: ({ color }) => (
+          <FontAwesome5 name="map-marked-alt" size={24} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="user" size={24} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+  </Tabs>
+);
 
 export default TabLayout;
