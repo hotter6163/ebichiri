440ca0712d83f73ef506eb191ac6a27b4dd97f25
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-04-08 10:55:18 +0900

    Update index.tsx

--- apps/expo/src/app/(tabs)/index.tsx
+++ apps/expo/src/app/(tabs)/index.tsx
@@ -2,10 +2,12 @@ import type { FC } from "react";
 import { Text } from "react-native";
 import { PageView } from "@/components/layout";
 
-const HomePage: FC = () => (
-  <PageView>
-    <Text className="text-white">Tab Home</Text>
-  </PageView>
-);
+const HomePage: FC = () => {
+  return (
+    <PageView>
+      <Text className="text-white">Tab Home</Text>
+    </PageView>
+  );
+};
 
 export default HomePage;
