78b698d85b74b88b3c12ad63f0ec7d1ed45a72d4
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-04-13 10:56:31 +0900

    UserProfileにフォロー数も追加

--- apps/expo/src/components/user.tsx
+++ apps/expo/src/components/user.tsx
@@ -5,16 +5,29 @@ import { Text, View } from "react-native";
 import { Avatar } from "./avatar";
 
 interface Props {
-  user?: RouterOutputs["user"]["getOneById"]["user"];
+  user?: NonNullable<RouterOutputs["user"]["getOneById"]>["user"] | null;
 }
 
 export const UserProfile: FC<Props> = ({ user }) => (
-  <View className="w-full gap-2 px-12 pt-4">
-    <View className="w-full flex-row items-center justify-between gap-2">
+  <View className="w-full gap-2 pt-4">
+    <View className="w-full flex-row items-center justify-between gap-8 pl-12 pr-4">
       <Avatar src={user?.avatar ?? undefined} />
-      <View className="flex-1"></View>
+      <View className=", flex-1 flex-row justify-center gap-4">
+        <View>
+          <Text className="text-white">フォロー数</Text>
+          <Text className="text-center text-xl font-bold text-white">
+            {user?.followingsCount}
+          </Text>
+        </View>
+        <View>
+          <Text className="text-white">フォロワー数</Text>
+          <Text className="text-center text-xl font-bold text-white">
+            {user?.followersCount}
+          </Text>
+        </View>
+      </View>
     </View>
-    <View className="w-full">
+    <View className="w-full px-12">
       <UserNameAndSlug user={user} />
     </View>
   </View>
@@ -23,7 +36,7 @@ export const UserProfile: FC<Props> = ({ user }) => (
 export const UserNameAndSlug: FC<Props> = ({ user }) => (
   <>
     <View className="h-7">
-      <Text className="text-lg font-bold text-white" adjustsFontSizeToFit>
+      <Text className="text-xl font-bold text-white" adjustsFontSizeToFit>
         {user?.name}
       </Text>
     </View>
